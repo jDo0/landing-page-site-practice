fb1b212101bc16c520b8b45a1bdb63ca1bdf4918
jDo0 <jackdolder1@gmail.com>
2023-04-09 16:50:45 +0100

    Add humburger menu JSX

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,7 @@ import { HiOutlineBars3 } from "react-icons/hi2"
 import { 
   Box, 
   Drawer, 
+  List, 
   ListItem, 
   ListItemButton, 
   ListItemIcon, 
@@ -58,10 +59,29 @@ const Navbar = () => {
         <button className="primary-button">
           Bookings Now
         </button>
-        <div className="navbar-menu-container">
-          <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
-        </div>
       </div>
+        <div className="navbar-menu-container">
+          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        </div>
+        <Drawer open={openMenu} onClose={() => setOpenMenu(false)} 
+        anchor="right">
+          <Box sx={{ width:250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+          >
+            <List>
+              {menuOptions.map((item) => (
+                <ListItem key={item.text} disablePadding >
+                  <ListItemButton>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text}></ListItemText>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
     </nav>
   )
 }
