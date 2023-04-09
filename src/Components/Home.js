6f1d9d3f42cde80f3f3cffc9c41a5d7bef0ba38a
jDo0 <jackdolder1@gmail.com>
2023-04-09 21:51:56 +0100

    Add home page text and order button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,6 +12,17 @@ const Home = () => {
           <div className="home-bannerImage-container">
             <img src={BannerBackground} alt=" banner background image" />
           </div>
+          <div className="home-text-section">
+            <h1 className="primary-heading">
+              Your Favourite Food Delivered Hot & Fresh
+            </h1>
+            <p className="primary-text">
+              Healthy switcher chefs do all the prep work, like peeling, chopping & marinating so you can cook fresh food.
+            </p>
+            <button className="secondary-button">
+              Order Now <FiArrowRight />
+            </button>
+          </div>
         </div>
     </div>
   )
