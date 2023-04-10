c08b2788955b922bb043e6c3a8e543cdfee93331
jDo0 <jackdolder1@gmail.com>
2023-04-10 14:52:40 +0100

    Add JSX to Work.js and render data array to page

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -22,7 +22,26 @@ const Work = () => {
         }
     ]
   return (
-    <div>Work</div>
+    <div className="work-section-wrapper">
+        <div className="work-section-top">
+            <p className="primary-subheading">Work</p>
+            <h1 className="primary-subheading">How It Works</h1>
+            <p className="primary-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quam obcaecati quod cum, deserunt necessitatibus doloremque voluptatibus tempore earum magni?</p>
+            <div className="work-section-bottom">
+                {
+                    workInfoData.map((data) => {
+                        <div className="work-section-info">
+                            <div className="info-boxes-img-container">
+                                <img src={data.image} alt={data.title} />
+                            </div>
+                            <h2>{data.title}</h2>
+                            <p>{data.text}</p>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 export default Work
