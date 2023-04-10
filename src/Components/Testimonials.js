bc3fa312d37326b908a99f0e8a27c880a75a30a8
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:15:24 +0100

    Create Testimonials.js file with initial imports

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ProfilePic from "../Assets/john-doe-image.png"
+import { AiFillStar } from "react-icons/ai"
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+export default Testimonials
