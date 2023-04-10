3a644f240a722f4a1c8ddffb6dab9d6bf5cab3dc
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:53:47 +0100

    Add page content to Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,39 @@ import { FaFacebookF } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div className="footer-wrapper">Footer</div>
+    <div className="footer-wrapper">
+        <div className="footer-section-one">
+            <div className="footer-logo-container">
+                <img src={Logo} alt="" />
+                <div className="footer-icons">
+                    <BsTwitter />
+                    <SiLinkedIn />
+                    <BsYoutube />
+                    <FaFacebookF />
+                </div>
+            </div>
+            <div className="footer-section-two">
+                <div className="footer-section-columns">
+                    <span>quality</span>
+                    <span>Help</span>
+                    <span>Share</span>
+                    <span>Careers</span>
+                    <span>Testimonials</span>
+                    <span>Work</span>
+                </div>
+                <div className="footer-section-columns">
+                    <span>244-5333-7783</span>
+                    <span>hello@food.com</span>
+                    <span>press@food.com</span>
+                    <span>contact@food.com</span>
+                </div>
+                <div className="footer-section-columns">
+                    <span>Terms & Conditions</span>
+                    <span>Privacy Policy</span>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 export default Footer
