103adaa9fa136a79b4967cee98ef844e749e994f
jDo0 <jackdolder1@gmail.com>
2023-04-10 14:44:26 +0100

    Add data array to Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -4,6 +4,23 @@ import ChooseMeals from "../Assets/choose-image.png"
 import DeliveryMeals from "../Assets/delivery-image.png"
 
 const Work = () => {
+    const workInfoData = [
+        {
+            image: PickMeals,
+            title: "Pick Meals",
+            text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et adipisci repellendus cumque?"
+        },
+        {
+            image: ChooseMeals,
+            title: "Choose How Often",
+            text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, nemo."
+        },
+        {
+            image: DeliveryMeals,
+            title: "Fast Deliveries",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing."
+        }
+    ]
   return (
     <div>Work</div>
   )
