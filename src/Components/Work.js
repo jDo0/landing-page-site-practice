7f0fda0df8c4e910fc413d3255fdbfbabe5b0a13
jDo0 <jackdolder1@gmail.com>
2023-04-10 14:38:54 +0100

    Create Work.js page with initial imports

---
+++ src/Components/Work.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PickMeals from "../Assets/pick-meals-image.png"
+import ChooseMeals from "../Assets/choose-image.png"
+import DeliveryMeals from "../Assets/delivery-image.png"
+
+const Work = () => {
+  return (
+    <div>Work</div>
+  )
+}
+export default Work
