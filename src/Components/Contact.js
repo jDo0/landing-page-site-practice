487ef252eee2e901f0d58b3c3a3e3b44f0af06c7
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:36:00 +0100

    Add content to Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,7 +2,14 @@ import React from "react"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact-page-wrapper">
+        <h1 className="primary-heading">Have A Question In Mind?</h1>
+        <h1 className="primary-heading">Let Us Help You</h1>
+        <div className="contact-form-container">
+            <input type="email" placeholder="youremail@email.com"/>
+            <button className="secondary-button">Submit</button>
+        </div>
+    </div>
   )
 }
 export default Contact
