87d0d23dea1eed8806108297c42997d18a3371eb
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:24:10 +0100

    Import and render Testimonials.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
 import Work from './Components/Work';
+import Testimonials from './Components/Testimonials';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Work />
+      <Testimonials />
     </div>
   );
 }
