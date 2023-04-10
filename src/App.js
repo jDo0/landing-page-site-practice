c5fd22d5309750d8b2930703357cce4449059361
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:37:05 +0100

    Import and render Contact.js to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Work from './Components/Work';
 import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Work />
       <Testimonials />
+      <Contact />
     </div>
   );
 }
