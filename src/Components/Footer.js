1bafce989cfd539061ffc8c9faf34f72509b650d
jDo0 <jackdolder1@gmail.com>
2023-04-10 15:45:18 +0100

    Create Footer.js with initial imports

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Logo from "../Assets/Logo.svg"
+import { BsTwitter } from "react-icons/bs"
+import { SiLinkedIn } from "react-icons/si"
+import { BsYoutube } from "react-icons/bs"
+import { FaFacebookF } from "react-icons/fa"
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">Footer</div>
+  )
+}
+export default Footer
